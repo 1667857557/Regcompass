d9f319fc47f0bc9258604947e55faeee4dc66540
Yu-Feng Huang <54109903+1667857557@users.noreply.github.com>
2026-07-15 23:21:52 +0800

    Preserve no-constraint medium scenarios during normalization

--- R/microcompass.R
+++ R/microcompass.R
@@ -54,7 +54,12 @@ rc_prepare_directional_targets <- function(gem, target_reactions,
   }
   if (!is.data.frame(medium_scenarios)) stop("`medium_scenarios` must be a data.frame.", call. = FALSE)
   if (!"medium_scenario_id" %in% colnames(medium_scenarios)) medium_scenarios$medium_scenario_id <- "custom"
-  medium_scenarios$.no_constraints <- FALSE
+  if (!".no_constraints" %in% colnames(medium_scenarios)) {
+    medium_scenarios$.no_constraints <- FALSE
+  } else {
+    medium_scenarios$.no_constraints <- as.logical(medium_scenarios$.no_constraints)
+    medium_scenarios$.no_constraints[is.na(medium_scenarios$.no_constraints)] <- FALSE
+  }
   medium_scenarios
 }
 
