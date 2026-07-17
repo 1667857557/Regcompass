fbc22ecb877b14de70c62a9f5dac6e827d224a87
Yu-Feng Huang <54109903+1667857557@users.noreply.github.com>
2026-07-17 10:47:49 +0800

    Index constrained bounds by reaction order

--- tests/testthat/test-required-result-fixes.R
+++ tests/testthat/test-required-result-fixes.R
@@ -141,8 +141,9 @@ test_that("COMPASS-style medium preserves model direction and caps exchanges", {
   ))
 
   constrained <- rc_apply_medium_constraints(gem, medium)
-  expect_equal(constrained$lb[["EX_blocked"]], 0)
-  expect_equal(constrained$ub[["EX_blocked"]], 0)
+  blocked_index <- match("EX_blocked", colnames(constrained$S))
+  expect_equal(constrained$lb[[blocked_index]], 0)
+  expect_equal(constrained$ub[[blocked_index]], 0)
 })
 
 test_that("COMPASS-style model bounds are the workflow defaults", {
