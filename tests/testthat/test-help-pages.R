9b36bb105e2490f63aec7de9b4cee2676c4cd01e
Yu-Feng Huang <54109903+1667857557@users.noreply.github.com>
2026-07-17 23:03:22 +0800

    Use testthat-compatible help assertions

--- tests/testthat/test-help-pages.R
+++ tests/testthat/test-help-pages.R
@@ -2,6 +2,6 @@ test_that("every exported function has an installed help topic", {
   exports <- getNamespaceExports("RegCompassR")
   for (name in exports) {
     topic <- utils::help(name, package = "RegCompassR")
-    expect_length(topic, 1L, info = paste("missing help for", name))
+    expect_length(topic, 1L)
   }
 })
