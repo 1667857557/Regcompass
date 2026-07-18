4de1332f426ddf6d2acf569e754462bfaa135925
Yu-Feng Huang <54109903+1667857557@users.noreply.github.com>
2026-07-18 15:41:26 +0800

    Use base numeric_version for SeuratObject compatibility

--- R/input_seurat.R
+++ R/input_seurat.R
@@ -1,9 +1,9 @@
 .rc_get_assay_counts <- function(object, assay) {
   seurat_object_version <- tryCatch(
     utils::packageVersion("SeuratObject"),
-    error = function(e) utils::package_version("0.0.0")
+    error = function(e) numeric_version("0.0.0")
   )
-  if (seurat_object_version >= utils::package_version("5.0.0")) {
+  if (seurat_object_version >= numeric_version("5.0.0")) {
     SeuratObject::GetAssayData(
       object = object,
       assay = assay,
