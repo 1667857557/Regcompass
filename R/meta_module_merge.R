ef9d6d7789207b6d38d724278a6cc757c22ceb97
Yu-Feng Huang <54109903+1667857557@users.noreply.github.com>
2026-07-26 21:36:16 +0800

    Carry multitask GRN provenance into merged meta-modules

--- R/meta_module_merge.R
+++ R/meta_module_merge.R
@@ -2,7 +2,11 @@
 
 .rc_merge_meta_module_catalogue <- function(condition_modules) {
   names_to_merge <- c(
-    "sample_status", "tf_peak_gene_all", "tf_peak_gene_significant",
+    "celltype_fit_status", "sample_status",
+    "tf_peak_gene_candidates", "tf_peak_gene_global",
+    "tf_peak_gene_condition_all", "tf_peak_gene_all",
+    "tf_peak_gene_significant", "condition_target_genes",
+    "target_model_diagnostics", "stability_diagnostics",
     "supported_metabolic_genes", "core_gene_reaction",
     "reaction_membership", "meta_module_summary"
   )
@@ -55,7 +59,15 @@
     ),
     stringsAsFactors = FALSE
   )
-  out$schema_version <- "regcompass_merged_meta_modules_v2"
+  multitask <- identical(
+    as.character(condition_modules$grn_mode %||% ""),
+    "multitask_shared_backbone"
+  )
+  out$schema_version <- if (multitask) {
+    "regcompass_merged_multitask_meta_modules_v1"
+  } else {
+    "regcompass_merged_meta_modules_v2"
+  }
   out$source_group_ids <- if (
     "group_id" %in% colnames(out$sample_status)
   ) {
@@ -63,9 +75,21 @@
   } else {
     character()
   }
-  out$core_definition <-
+  out$source_edge_universe_ids <- if (
+    "edge_universe_id" %in% colnames(out$tf_peak_gene_candidates)
+  ) {
+    unique(as.character(out$tf_peak_gene_candidates$edge_universe_id))
+  } else {
+    character()
+  }
+  out$core_definition <- if (multitask) {
+    "condition_celltype_stability_selected_subgrn_targets_complete_gpr"
+  } else {
     "condition_celltype_significant_pando_targets_complete_gpr"
+  }
   out$merge_source <- "deduplicated_biological_meta_module_reactions"
+  out$structural_model_policy <-
+    "condition modules are merged before one medium-specific union GEM is built"
   out$is_gem <- FALSE
   out$fastcore_applied <- FALSE
   out
